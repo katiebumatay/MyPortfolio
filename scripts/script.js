03ac975bda796476629ecc930c45d0febf94e649
Katie Bumatay <bumatay.katie@gmail.com>
2019-09-23 13:18:44 -0500

    add license key for fullpage

--- scripts/script.js
+++ scripts/script.js
@@ -237,6 +237,9 @@ function navHideShow() {
 // $('#fullpage').fullpage();
 
 new fullpage('#fullpage', {
+
+	licenseKey: 'A82ACDA6-81874E36-8100F2A7-1F26EC7D'
+
 	// anchors: ['page-about', 'page-landing', 'page-1', 'page-2'],
 	onLeave: function(origin, destination, direction){
 		prevSection = this;
