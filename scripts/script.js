55cbcf8dc33034ff0712bf31eafe9ffa874ffee8
Katie Bumatay <bumatay.katie@gmail.com>
2022-08-01 16:55:49 -0400

    remove img switch functionality and clean js

--- scripts/script.js
+++ scripts/script.js
@@ -1,73 +1,8 @@
 // variables to track mouse movement
 var oldX = 0; var oldY = 0; var newX = 0; var newY = 0;
 
-// variables to choose which project to display
-var proj; var projChoice;
-
 // variables to choose which about picture to display
 var abtPic; var abtPicChoice;
-
-// variables for tracking sections
-var numSections; var currentSection; var prevSection;
-
-// variables for links to project pages
-const proj1 = "and-justice-for-all";
-const proj2 = "glitchin-gifs";
-const proj3 = "a-type-of-mosaic";
-const proj4 = "metamorph-typeface";
-const proj5 = "pleasures-of-the-door";
-var projectLink;
-
-// variable for controlling animation of scroll indicator
-var scrollTimer;
-
-// check if device is mobile
-var isMobile = false;
-if (window.innerWidth <= 480) {
-	isMobile = true;
-}
-
-// randomly select project to display when page loads
-function setProj() {
-		proj = Math.floor(Math.random() * 3) + 1;
-		projChoice = "projects/" + proj + "/cover-bw.jpg";
-		setProjLink();
-    	$("#proj-img-switch").attr("src", projChoice);
-    	$("#tooltip-switch").text(projectLink);
-    	console.log("setProj() ran and project choice is " + projChoice);
-};
-
-// randomly select project to display when mouse moves more than 40 pixels
-function chooseProj() {
-	if ((newY > (oldY + 100)) || (newY < (oldY - 100)) || (newX > (oldX + 100)) || (newX < (oldX - 100))) {
-		setProj();
-    	oldX = newX;
-    	oldY = newY;
-	}
-};
-
-// set link to project based on which cover image is displayed
-function setProjLink() {
-	if (proj == 1) {
-		projectLink = proj1;
-	}
-	else if (proj == 2) {
-		projectLink = proj2;
-	}
-	else if (proj == 3) {
-		projectLink = proj3;
-	}
-	else if (proj == 4) {
-		projectLink = proj4;
-	}
-	else if (proj == 5) {
-		projectLink = proj5;
-	}
-	else if (proj == 6) {
-		projectLink = proj6;
-	}
-	projectLink = projectLink;
-}
 
 // randomly select an about picture to display
 function setAbtPic() {
@@ -100,6 +35,7 @@ function topFunction() {
 // after page loads
 $(document).ready(function() {
 
+	//Nav animation when scrolling
 	var prevScrollpos = window.pageYOffset;
 	window.onscroll = function() {
 		var currentScrollPos = window.pageYOffset;
@@ -125,13 +61,5 @@ $(document).ready(function() {
 		chooseAbtPic();
 		// setAbtPic();
 	});
-
-    // track mouse movement inside home image and change project picture
-	$(".proj-img-switch-container").mousemove(function(event){
-		newX = event.pageX;
-		newY = event.pageY;
-		chooseProj();
-	});
-	 
 
 });
